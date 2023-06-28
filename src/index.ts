a032e821e342e3d1abeaf4d581505727c8e51a90
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-06-28 13:32:02 +0800

    second commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import Game from "./core/desktop/Game"
+// import GameMobile from "./core/mobile/Game"
+import "./main.css"
+
+// new Game();
+// document.getElementsByTagName("body")[0].classList.add("desktop");
+// document.getElementsByTagName("canvas")[0].classList.add("desktop");
